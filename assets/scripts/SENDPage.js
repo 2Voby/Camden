34fa3f5b5d463167e4d2476f2a0b19d7b901bd83
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2025-02-15 19:06:28 +0200

    Add files via upload

--- assets/scripts/SENDPage.js
+++ assets/scripts/SENDPage.js
@@ -1,6 +1,7 @@
 const { createApp, ref } = Vue;
 document.addEventListener("DOMContentLoaded", async function () {
 	const response = await fetch("./../schools.json");
+
 	let schools = await response.json();
 	const urlParams = new URLSearchParams(window.location.search);
 
@@ -15,21 +16,9 @@ document.addEventListener("DOMContentLoaded", async function () {
 		schoolId = 0;
 		location.href = `${location.origin}`;
 	}
-	let showComparisonsInfo = ref(false);
-
 	createApp({
 		setup() {
-			return { school, showComparisonsInfo };
+			return { school };
 		},
-	}).mount("#achievementPage");
-
-	const checkbox = document.querySelector("#switch1");
-
-	checkbox.addEventListener("change", function () {
-		if (checkbox.checked) {
-			showComparisonsInfo.value = true;
-		} else {
-			showComparisonsInfo.value = false;
-		}
-	});
+	}).mount("#sendPage");
 });
--- assets/scripts/mainPage.js
+++ assets/scripts/mainPage.js
@@ -1,15 +1,19 @@
 import { createTab } from "./modules/tabs.js";
 const { createApp, ref } = Vue;
-// let schools = ref([{ title: "School" }, { title: "School1" }, { title: "School2" }, { title: "School3" }]);
+
+let map = null;
+let schoolMarkers = [];
+
 document.addEventListener("DOMContentLoaded", async function () {
 	const response = await fetch("./schools.json");
 	const staticSchools = await response.json();
 	let schools = ref(staticSchools);
+	let filterOverlayData = ref(getFiltersFields(staticSchools));
 
 	// Vue.js app initialization
 	createApp({
 		setup() {
-			return { schools };
+			return { schools, filterOverlayData };
 		},
 	}).mount("#mainPage");
 
@@ -19,36 +23,37 @@ document.addEventListener("DOMContentLoaded", async function () {
 		createTab(mainPageTabItem);
 	}
 
+	// init view state
+	handleViewChange(staticSchools);
+	schools.value = handleFilters(staticSchools);
+
+	// add listener on search line to change view and filters
+	window.addEventListener("popstate", () => {
+		handleViewChange(staticSchools);
+		schools.value = handleFilters(staticSchools);
+	});
+
 	// Main page navigation button (change list to map)
 	let navButton = document.querySelector(".navigation__button");
 	if (navButton) {
-		let mapBlock = document.querySelector(".schools-info__map");
-		let listBlock = document.querySelector(".schools-info__list");
 		navButton.addEventListener("click", function () {
-			if (navButton.classList.contains("map")) {
-				navButton.classList.remove("map");
-				navButton.classList.add("list");
-				mapBlock.classList.add("_hidden");
-				listBlock.classList.remove("_hidden");
-				navButton.innerHTML = `<img src="./assets/imgs/map-icon.png" alt="" /><span>Map</span>`;
-			} else if (navButton.classList.contains("list")) {
-				navButton.classList.add("map");
-				navButton.classList.remove("list");
-				mapBlock.classList.remove("_hidden");
-				listBlock.classList.add("_hidden");
-				navButton.innerHTML = `<img src="./assets/imgs/list-icon.png" alt="" /><span>List</span>`;
-				drawMap(staticSchools);
-			}
+			const urlParams = new URLSearchParams(window.location.search);
+			let newView = navButton.classList.contains("map") ? "list" : "map";
+			urlParams.set("view", newView);
+
+			window.history.pushState({}, "", `${window.location.pathname}?${urlParams.toString()}`);
+
+			handleViewChange(staticSchools);
 		});
 	}
 
 	// search by name or post code
-
 	document.getElementById("searchSchoolInput")?.addEventListener("input", (event) => {
-		schools.value = filterSchools(event.target.value, staticSchools);
+		schools.value = findSchools(event.target.value, staticSchools);
+		drawMap(schools.value);
 	});
 
-	// Filter overlay setup
+	// FilterOverlay setup
 	let openFilterOverlayBtn = document.querySelector(".schools-filter__button");
 	if (openFilterOverlayBtn) {
 		openFilterOverlayBtn.addEventListener("click", openFilterOverlay);
@@ -69,7 +74,67 @@ document.addEventListener("DOMContentLoaded", async function () {
 			document.querySelector(".age-range .minValue").innerText = ageRangeInput.value;
 		});
 	}
+
+	// applyFilters
+	let applyFiltersButton = document.querySelector(".filter-accept");
+	if (applyFiltersButton) {
+		applyFiltersButton.addEventListener("click", function () {
+			schools.value = filterSchools(staticSchools);
+			closeFilterOverlay();
+		});
+	}
+	// resetFilters
+	let resetFiltersButton = document.querySelector(".filter-clear");
+	if (resetFiltersButton) {
+		resetFiltersButton.addEventListener("click", function () {
+			schools.value = staticSchools;
+			clearFilerOverlayCheckboxes();
+			closeFilterOverlay();
+		});
+	}
 });
+
+// change view
+function handleViewChange(staticSchools) {
+	const urlParams = new URLSearchParams(window.location.search);
+	let viewType = urlParams.get("view") || "list";
+
+	let navButton = document.querySelector(".navigation__button");
+	let mapBlock = document.querySelector(".schools-info__map");
+	let listBlock = document.querySelector(".schools-info__list");
+
+	if (viewType === "map") {
+		navButton.classList.add("map");
+		navButton.classList.remove("list");
+		mapBlock.classList.remove("_hidden");
+		listBlock.classList.add("_hidden");
+		navButton.innerHTML = `<img src="./assets/imgs/list-icon.png" alt="" /><span>List</span>`;
+		drawMap(staticSchools);
+	} else {
+		navButton.classList.remove("map");
+		navButton.classList.add("list");
+		mapBlock.classList.add("_hidden");
+		listBlock.classList.remove("_hidden");
+		navButton.innerHTML = `<img src="./assets/imgs/map-icon.png" alt="" /><span>Map</span>`;
+	}
+}
+
+function handleFilters(schools) {
+	const urlParams = new URLSearchParams(window.location.search);
+	let phase = urlParams.get("phase") || null;
+
+	if (phase == "primary") {
+		return schools.filter((school) => {
+			return school.isPrimary == true;
+		});
+	} else if (phase == "secondary") {
+		return schools.filter((school) => {
+			return school.isPrimary == false;
+		});
+	} else {
+		return schools;
+	}
+}
 
 // Close filter overlay function
 function closeFilterOverlay() {
@@ -91,7 +156,79 @@ function openFilterOverlay() {
 	filterOverlay.classList.add("_active");
 }
 
-function filterSchools(query, schools) {
+function clearFilerOverlayCheckboxes() {
+	let filterOverlay = document.querySelector(".filter-overlay");
+	if (filterOverlay) {
+		let checkboxes = filterOverlay.querySelectorAll("input[type=checkbox]");
+		for (const input of checkboxes) {
+			input.checked = false;
+		}
+		let ageRangeInput = filterOverlay.querySelector(".age-range-input");
+		if (ageRangeInput) {
+			ageRangeInput.value = ageRangeInput.min;
+			document.querySelector(".age-range .minValue").innerText = ageRangeInput.value;
+		}
+	}
+}
+
+// filter schools
+function filterSchools(schools) {
+	let ageRangeValue = document.querySelector(".age-range-input");
+	if (ageRangeValue) {
+		ageRangeValue = +ageRangeValue.value;
+	}
+
+	let schoolTypeValues = [];
+	document.querySelectorAll(".school-type-filters button").forEach((button) => {
+		let buttonScheckbox = button.querySelector('input[type="checkbox"]');
+		if (buttonScheckbox && buttonScheckbox.checked == true) {
+			schoolTypeValues.push(button.innerText);
+		}
+	});
+
+	let genderValues = [];
+	document.querySelectorAll(".genders-filters button").forEach((button) => {
+		let buttonScheckbox = button.querySelector('input[type="checkbox"]');
+		if (buttonScheckbox && buttonScheckbox.checked == true) {
+			genderValues.push(button.innerText);
+		}
+	});
+
+	let facilitiesValues = [];
+	document.querySelectorAll(".facilities-filter button").forEach((button) => {
+		let buttonScheckbox = button.querySelector('input[type="checkbox"]');
+		if (buttonScheckbox && buttonScheckbox.checked == true) {
+			facilitiesValues.push(button.innerText);
+		}
+	});
+
+	let minSchoolsAge = 100;
+	schools.forEach((school) => {
+		const [minAge, maxAge] = school.ageRange;
+		if (minSchoolsAge > minAge) {
+			minSchoolsAge = minAge;
+		}
+	});
+
+	let filteredSchools = schools.filter((school) => {
+		const [minAge, maxAge] = school.ageRange;
+
+		let ageMatch = ageRangeValue >= minAge && ageRangeValue <= maxAge;
+		if (ageRangeValue == minSchoolsAge) {
+			ageMatch = true;
+		}
+
+		const facilitiesMatch = facilitiesValues.length === 0 || facilitiesValues.some((type) => school.facilities.includes(type));
+		const genderMatch = genderValues.length === 0 || genderValues.includes(school.gender);
+		const schoolTypeMatch = schoolTypeValues.length === 0 || schoolTypeValues.includes(school.type);
+
+		return ageMatch && facilitiesMatch && genderMatch && schoolTypeMatch;
+	});
+
+	return filteredSchools;
+}
+
+function findSchools(query, schools) {
 	const lowerQuery = query.toLowerCase();
 
 	const filteredSchools = schools.filter((school) => {
@@ -105,17 +242,26 @@ function filterSchools(query, schools) {
 }
 
 function drawMap(staticSchools) {
-	let map = L.map("map", {
-		zoomControl: false,
-		attributionControl: false,
-	}).setView([51.50603, -0.12285], 11);
+	if (!map) {
+		map = L.map("map", {
+			zoomControl: false,
+			attributionControl: false,
+		}).setView([51.50603, -0.12285], 11);
 
-	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	}).addTo(map);
+		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
+	}
+
+	redrawSchools(staticSchools);
+}
+
+function redrawSchools(staticSchools) {
+	schoolMarkers.forEach((marker) => map.removeLayer(marker));
+	schoolMarkers = [];
 
 	staticSchools.forEach((school) => {
-		L.circleMarker([school.lat, school.long], {
+		let marker = L.circleMarker([school.lat, school.long], {
 			color: "white",
 			fillColor: "#ff0077",
 			fillOpacity: 1,
@@ -123,32 +269,60 @@ function drawMap(staticSchools) {
 		})
 			.bindPopup(
 				`<div class="schools-list_item school-item school-item-as-popup">
-					<div class="school-item__header">
-						<img src="./assets/imgs/school-logo.png" alt="school-logo" />
-						<h4 class="school-item__name">Demo School</h4>
-					</div>
-					<p class="school-itemy__description">We want our pupils to become informed, articulate and empowered citizens.</p>
-					<div class="school-item__info-row info-row">
-						<div class="info-row__item">
-							<p>Age Range</p>
-							<span>4-11</span>
-						</div>
-						<div class="info-row__item">
-							<p>School Type</p>
-							<span>Roman Catholic</span>
-						</div>
-						<div class="info-row__item">
-							<p>Sex</p>
-							<span>Mixed</span>
-						</div>
-					</div>
-					<div class="school-item__footer">
-						<a class="link" href="/Camden/school?schoolId=${school.schoolId}"
-							><span>View school details</span> <img src="./assets/imgs/arrow-outward.png" alt="arrow"
-						/></a>
-					</div>
-				</div>`
+                    <div class="school-item__header">
+                        <img src="${school.schoolLogo}" alt="school-logo" />
+                        <h4 class="school-item__name">${school.schoolName}</h4>
+                    </div>
+                    <p class="school-itemy__description">${school.shortSlogan}</p>
+                    <div class="school-item__info-row info-row">
+                        <div class="info-row__item">
+                            <p>Age Range</p>
+                            <span>${school.ageRange}</span>
+                        </div>
+                        <div class="info-row__item">
+                            <p>School Type</p>
+                            <span>${school.type}</span>
+                        </div>
+                        <div class="info-row__item">
+                            <p>Sex</p>
+                            <span>${school.gender}</span>
+                        </div>
+                    </div>
+                    <div class="school-item__footer">
+                        <a class="link" href="/school?schoolId=${school.schoolId}">
+                            <span>View school details</span> 
+                            <img src="./assets/imgs/arrow-outward.png" alt="arrow" />
+                        </a>
+                    </div>
+                </div>`
 			)
 			.addTo(map);
+
+		schoolMarkers.push(marker);
 	});
+}
+
+function getFiltersFields(schools) {
+	// get schools age ranges
+	const ageRanges = schools.map((school) => school.ageRange);
+	const minAge = Math.min(...ageRanges.map((range) => range[0]));
+	const maxAge = Math.max(...ageRanges.map((range) => range[1]));
+
+	// get schools types
+	const schoolTypes = [...new Set(schools.map((school) => school.type))];
+
+	// get schools genders
+	const schoolGenders = [
+		...new Set(
+			schools.map((school) => {
+				return school.gender;
+			})
+		),
+	];
+
+	// get schools facilities
+	let schoolFacilities = [...new Set(schools.flatMap((school) => school.facilities))];
+	// console.log(schoolFacilities);
+
+	return { ageRange: [minAge, maxAge], schoolTypes: schoolTypes, genders: schoolGenders, facilities: schoolFacilities };
 }
